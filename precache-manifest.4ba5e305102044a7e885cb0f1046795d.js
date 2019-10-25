8a50a8d375b37ce2f7d689789eb44117fbb69abf
lightlyss <37601555+lightlyss@users.noreply.github.com>
2019-10-25 22:45:27 +0000

    Deploying to gh-pages from master 35003ebc8abc01aa4b76c350bd6fe4a0e8247422

---
+++ precache-manifest.4ba5e305102044a7e885cb0f1046795d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31c2ded236a5e8624db5a4ffe9320b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09674dd706f04f775024",
+    "url": "./static/css/2.e579572b.chunk.css"
+  },
+  {
+    "revision": "8e5325d2de4e41fa0022",
+    "url": "./static/css/main.13254d15.chunk.css"
+  },
+  {
+    "revision": "09674dd706f04f775024",
+    "url": "./static/js/2.d1b31ab6.chunk.js"
+  },
+  {
+    "revision": "8e5325d2de4e41fa0022",
+    "url": "./static/js/main.0073f2ff.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import './index.css';
-import 'yorha/dist/yorha.min.css';
-
-const params = new URLSearchParams(window.location.search);
-ReactDOM.render(<App model={params.get('model')}/>, document.getElementById('root'));
